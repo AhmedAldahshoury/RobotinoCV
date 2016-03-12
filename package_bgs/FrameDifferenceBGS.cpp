733c1e471fa3720f20b8122837e9aab21ac75de4
Ahmed Aldahshoury <ahmed.aldahshoury@gmail.com>
2016-03-12 22:00:04 +0200

    frame difference "using cam and avi "!!

--- package_bgs/FrameDifferenceBGS.cpp
+++ package_bgs/FrameDifferenceBGS.cpp
@@ -62,7 +62,7 @@ void FrameDifferenceBGS::process(const cv::Mat &img_input, cv::Mat &img_output, 
 
 void FrameDifferenceBGS::saveConfig()
 {
-  CvFileStorage* fs = cvOpenFileStorage("./config/FrameDifferenceBGS.xml", 0, CV_STORAGE_WRITE);
+  CvFileStorage* fs = cvOpenFileStorage("/home/ahmed/RobotinoCV/config/FrameDifferenceBGS.xml", 0, CV_STORAGE_WRITE);
 
   cvWriteInt(fs, "enableThreshold", enableThreshold);
   cvWriteInt(fs, "threshold", threshold);
@@ -73,8 +73,8 @@ void FrameDifferenceBGS::saveConfig()
 
 void FrameDifferenceBGS::loadConfig()
 {
-  CvFileStorage* fs = cvOpenFileStorage("./config/FrameDifferenceBGS.xml", 0, CV_STORAGE_READ);
-  
+  CvFileStorage* fs = cvOpenFileStorage("/home/ahmed/RobotinoCV/config/FrameDifferenceBGS.xml", 0, CV_STORAGE_READ);
+
   enableThreshold = cvReadIntByName(fs, 0, "enableThreshold", true);
   threshold = cvReadIntByName(fs, 0, "threshold", 15);
   showOutput = cvReadIntByName(fs, 0, "showOutput", true);

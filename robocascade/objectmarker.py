1f54d57d68bb1b812ebe637bad84f6aca07392b2
Ahmed Aldahshoury <ahmed.aldahshoury@gmail.com>
2016-03-27 22:14:06 +0200

    adding robocascade V 0.1

--- robocascade/objectmarker.py
+++ robocascade/objectmarker.py
@@ -2,10 +2,10 @@
 
 ###############################################################################
 # Name		: ObjectMarker.py
-# Author	: Python implementation: sqshemet 
+# Author	: Python implementation: sqshemet
 # 	 	  Original ObjectMarker.cpp: http://www.cs.utah.edu/~turcsans/DUC_files/HaarTraining/
 # Date		: 7/24/12
-# Description	: Object marker utility to be used with OpenCV Haar training. 
+# Description	: Object marker utility to be used with OpenCV Haar training.
 #		  Tested on Ubuntu Linux 10.04 with OpenCV 2.1.0.
 # Usage		: python ObjectMarker.py outputfile inputdirectory
 ###############################################################################
@@ -19,7 +19,7 @@ IMG_CHAN = 3
 IMG_DEPTH = cv.IPL_DEPTH_8U
 image = cv.CreateImage(IMG_SIZE, IMG_DEPTH, IMG_CHAN)
 lol = cv.CreateImage(IMG_SIZE, IMG_DEPTH, IMG_CHAN)
-image2 = cv.CreateImage(IMG_SIZE, IMG_DEPTH, IMG_CHAN) 
+image2 = cv.CreateImage(IMG_SIZE, IMG_DEPTH, IMG_CHAN)
 roi_x0 = 0
 roi_y0 = 0
 roi_x1 = 0
@@ -46,7 +46,7 @@ def on_mouse(event, x, y, flag, params):
 	elif (event == cv.CV_EVENT_MOUSEMOVE and start_draw):
 		#Redraw ROI selection
 		image2 = cv.CloneImage(image)
-		cv.Rectangle(image2, (roi_x0, roi_y0), (x,y), 
+		cv.Rectangle(image2, (roi_x0, roi_y0), (x,y),
 			cv.CV_RGB(255,0,255),36)
 		cv.ShowImage(window_name, image2)
 
@@ -54,27 +54,27 @@ def main():
 
 	global image
 	iKey = 0
-	
+
 	if (len(sys.argv) != 3):
-		sys.stderr.write("%s output_info.txt raw/data/directory\n" 
+		sys.stderr.write("%s output_info.txt raw/data/directory\n"
 			% sys.argv[0])
 		return -1
 
 	input_directory = sys.argv[2]
 	output_file = sys.argv[1]
-	
+
 
 	#Get a file listing of all files within the input directory
 	try:
 		files = os.listdir(input_directory)
 	except OSError:
-		sys.stderr.write("Failed to open dirctory %s\n" 
+		sys.stderr.write("Failed to open dirctory %s\n"
 			% input_directory)
 		return -1
 
 	files.sort()
 
-	sys.stderr.write("ObjectMarker: Input Directory: %s Output File %s\n" 
+	sys.stderr.write("ObjectMarker: Input Directory: %s Output File %s\n"
 			% (input_directory, output_file))
 
 	# init GUI
@@ -92,18 +92,18 @@ def main():
 	except IOError:
 		sys.stderr.write("Failed to open file %s.\n" % output_file)
 		return -1
-        
+
         del files[0];
-        
+
 	for file in files:
 		str_postfix = ""
 		num_of_rec = 0
                 img = str_prefix + "/" +  file
 		sys.stderr.write("Loading image %s...\n" % img)
 
-		try: 
+		try:
 			image = cv.LoadImage(img, 1)
-		except IOError: 
+		except IOError:
 			sys.stderr.write("Failed to load image %s.\n" % img)
 			return -1
 
@@ -122,16 +122,16 @@ def main():
 			num_of_rec += 1
 			# Currently two draw directions possible:
 			# from top left to bottom right or vice versa
+			# from top left to bottom right or vice versa
 			if (roi_x0<roi_x1 and roi_y0<roi_y1):
 				str_postfix += " %d %d %d %d\n" % (roi_x0,
 					roi_y0, (roi_x1-roi_x0), (roi_y1-roi_y0))
 			elif (roi_x0>roi_x1 and roi_y0>roi_y1):
-				str_postfix += " %d %d %d %d\n" % (roi_x1, 
+				str_postfix += " %d %d %d %d\n" % (roi_x1,
 					roi_y1, (roi_x0-roi_x1), (roi_y0-roi_y1))
 			output.write(img + " " + str(num_of_rec) + str_postfix)
-			print("a7a");
 		if iKey == 120:
 			sys.stderr.write("Skipped %s.\n" % img)
-		
+
 if __name__ == '__main__':
-	main() 
+	main()
